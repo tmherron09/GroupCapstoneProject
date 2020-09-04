62b62601a45e453936ce2f2701e612da093597e0
Timsy Magnus <tmherron09@gmail.com>
2020-09-04 11:49:08 -0500

    Init Commit

---
+++ GadeliniumGroupCapstone/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace GadeliniumGroupCapstone.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
