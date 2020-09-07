44e40d94834e2c264b22526b2623689fe3bcda90
Milan Stojanovic <milanstoj94@gmail.com>
2020-09-06 23:17:58 -0500

    added use-statements for key attributes

--- GadeliniumGroupCapstone/Models/Sitter.cs
+++ GadeliniumGroupCapstone/Models/Sitter.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,6 +9,7 @@ namespace GadeliniumGroupCapstone.Models
 {
     public class Sitter
     {
+        [Key]
         public int SitterId { get; set; }
 
         [ForeignKey("Business")]
